98529d179d421750f92236238cf2ee6c5bb6ae4e
Paul Biberstein <pbibersteinintern@jumptrading.com>
2022-06-27 19:54:05 +0000

    began work on frontend registration

---
+++ frontend/components/metamaskButton.tsx
@@ -0,0 +1,32 @@
+import * as ethers from "ethers";
+
+type MetamaskButtonProps = {
+  provider: ethers.providers.Web3Provider;
+  setUserAddress: (_: string) => void;
+};
+
+const MetamaskButton = ({ provider, setUserAddress}: MetamaskButtonProps) => {
+  const requestAccount = async () => {
+    let accounts = await provider.send("eth_requestAccounts", []);
+    console.log("accounts", accounts);
+    setUserAddress(accounts[0])
+    console.log(provider);
+  };
+
+  if (typeof (window as any).ethereum !== "undefined") {
+    return (
+      <div>
+        <button onClick={(_ => requestAccount())} className="btn btn-blue">Connect Wallet</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button className="btn btn-disabled">Connect Wallet</button>
+        <p className="text-red">Wallet provider not detected</p>
+      </div>
+    );
+  }
+};
+
+export default MetamaskButton;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,26 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { readFileSync } from "fs";
-import {providers, Contract} from "ethers";
-import { useEffect, useState } from "react";
+import * as ethers from "ethers";
+import { useEffect, useMemo, useState } from "react";
+import dynamic from "next/dynamic";
+import TokenSelector from "../components/tokenSelector";
 
+const MetamaskButton = dynamic(() => import("../components/metamaskButton"), {
+  ssr: false,
+});
+
+type Network = {
+  type: string;
+  wormholeChainId: number;
+  rpc: string;
+  privateKey: string;
+  bridgeAddress: string;
+  deployedAddress: string;
+  emittedVAAs: string;
+};
 type Config = {
-  networks: Record<string, {
-    type: string;
-    wormholeChainId: number;
-    rpc: string;
-    privateKey: string;
-    bridgeAddress: string;
-    deployedAddress: string;
-    emittedVAAs: string;
-  }>;
+  networks: Record<string, Network>;
   wormhole: {
     restAddress: string;
   };
@@ -32,45 +39,70 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: {
-      networks,
+      network: networks["evm0"],
       abi,
     },
   };
 };
 
 type HomeProps = {
-  networks: Config["networks"];
+  network: Network;
   abi: any;
 };
 
-const Home: NextPage<HomeProps> = ({ networks, abi }) => {
-  const [contractReturnValue, setContractReturnValue] = useState(null);
+const Home: NextPage<HomeProps> = ({ network, abi }) => {
+  console.log(network);
 
-  const getOnChainValue = async () => {
-    const url = networks["evm0"].rpc;
-    const provider = new providers.JsonRpcProvider(url);
-    const contract = new Contract(
-      networks["evm0"].deployedAddress,
-      abi,
-      provider
-    );
-
-    const value = await contract.getValue();
-    setContractReturnValue(value);
-  };
-
+  const [provider, setProvider] =
+    useState<ethers.providers.Web3Provider | null>(null);
   useEffect(() => {
-    getOnChainValue();
-  });
+    if (process.browser === false) {
+      return;
+    }
+    setProvider(new ethers.providers.Web3Provider((window as any).ethereum));
+  }, []);
+
+  const [userAddress, setUserAddress] = useState<string | null>(null);
+
+  // const [contractReturnValue, setContractReturnValue] = useState<any | null>(null);
+  // const getOnChainValue = async () => {
+  //   if (userAddress === "") {
+  //     return;
+  //   }
+
+  //   const url = network.rpc;
+  //   const provider = new ethers.providers.JsonRpcProvider(url);
+  //   const contract = new ethers.Contract(
+  //     network.deployedAddress,
+  //     abi,
+  //     provider
+  //   );
+
+  //   const value = await contract.getValue();
+  //   setContractReturnValue(value);
+  // };
+
+  // useEffect(() => {
+  //   getOnChainValue();
+  // }, [userAddress]);
 
   return (
-    <div>
+    <div className="flex flex-row items-center justify-center w-screen h-screen p-0 m-0 align-center">
       <Head>
         <title>Champion Draft</title>
         <meta name="description" content="Champion Draft" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>{JSON.stringify(contractReturnValue)}</p>
+      {provider === null ? (
+        "Loading..."
+      ) : userAddress === null ? (
+        <MetamaskButton
+          provider={provider}
+          setUserAddress={(a: string) => setUserAddress(a)}
+        />
+      ) : (
+        <TokenSelector provider={provider} />
+      )}
     </div>
   );
 };
