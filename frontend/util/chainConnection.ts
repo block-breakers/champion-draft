3b5cb4451c18a8a70918ba3b6149995d9e99d49c
Paul Biberstein <pbibersteinintern@jumptrading.com>
2022-06-30 20:24:36 +0000

    fix user chain detection

--- frontend/util/chainConnection.ts
+++ frontend/util/chainConnection.ts
@@ -3,11 +3,24 @@ import { ethers } from "ethers";
 export const getUsersNetworkIdentifier = async (
   provider: ethers.providers.Web3Provider
 ) => {
-  if (await provider.send("net_version", []) === "1656598392803") {
+  if (
+    (await provider.send("net_version", [])) ===
+    (await new ethers.providers.JsonRpcProvider("http://localhost:8545").send(
+      "net_version",
+      []
+    ))
+  ) {
     return "evm0";
-  } else if (await provider.send("net_version", []) === "1656598393535") {
+  } else if (
+    (await provider.send("net_version", [])) ===
+    (await new ethers.providers.JsonRpcProvider("http://localhost:8546").send(
+      "net_version",
+      []
+    ))
+  ) {
     return "evm1";
   } else {
     throw new Error("Unrecognized chain");
   }
 };
+
