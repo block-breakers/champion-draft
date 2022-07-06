3d2cfec4dbdca195b9ca1a6b34dc8568e86aaacf
Steven Yao <skyao2002@gmail.com>
2022-07-06 19:53:29 +0000

    add server to track champion hash for evm

---
+++ server/server.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import redis
+import json
+import asyncio
+from listener import EVMListener
+
+app = Flask(__name__)
+
+f = open("../xdapp.config.json")
+config = json.load(f)
+
+abiPath = "../chains/evm/out/CoreGame.sol/CoreGame.json"
+abiFile = open(abiPath)
+abi = json.load(abiFile)["abi"]
+
+redisDatabase = redis.Redis(host='localhost', port=6379)
+
+chainListeners = {}
+
+def setup(name):
+    chainConfig = config["networks"][name]
+
+    listener = EVMListener(
+        f"{name}-champions",
+        chainConfig["rpc"], 
+        chainConfig["deployedAddress"], 
+        abi,
+        redisDatabase)
+
+    chainListeners[name] = listener
+
+evm0 = "evm0"
+setup(evm0)
+
+@app.route("/healthz")
+def healthz():
+    return "<p>Server up and running!</p>"
+
+@app.route("/champions")
+def champions():
+    chainName = request.args.get('chain')
+    prevListLength = request.args.get('idx')
+    if chainName not in chainListeners:
+        return jsonify([])
+    
+    return jsonify(chainListeners[chainName].getChampions(prevListLength))
