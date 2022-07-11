b4254d30af5f32ffba2fef0ee5013224732b5eba
Paul Biberstein <pbibersteinintern@jumptrading.com>
2022-07-11 21:55:43 +0000

    Basic example of submitting VAA on solana

--- chains/solana/tests/core_game.ts
+++ chains/solana/tests/core_game.ts
@@ -20,26 +20,21 @@ import {
   parseSequencesFromLogSolana,
   setDefaultWasm,
   tryNativeToHexString,
+  postVaaSolanaWithRetry,
+  importCoreWasm,
+  getEmitterAddressEth,
 } from "@certusone/wormhole-sdk";
 import axios from "axios";
 import { WormholeSolanaSdk } from "../target/types/wormhole_solana_sdk";
+import * as b from "byteify";
 
 setDefaultWasm("node");
 
+const SOLANA_CORE_BRIDGE_ADDRESS =
+  "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o";
+
 function getVaaBody(signedVaa: Buffer): Buffer {
   return signedVaa.subarray(57 + 66 * signedVaa[5]);
-}
-
-function parseSaleId(iccoVaa: Buffer): Buffer {
-  return getVaaBody(iccoVaa).subarray(1, 33);
-}
-
-function hashVaaPayload(signedVaa: Buffer): Buffer {
-  const sigStart = 6;
-  const numSigners = signedVaa[5];
-  const sigLength = 66;
-  const bodyStart = sigStart + sigLength * numSigners;
-  return keccak256(signedVaa.subarray(bodyStart));
 }
 
 class Orchestrator {
@@ -73,13 +68,10 @@ class Orchestrator {
       [Buffer.from("emitter")],
       program.programId
     );
-    console.log("emitter", wormholeEmitter.toString());
     const wormholeSequence = deriveAddress(
       [Buffer.from("Sequence"), wormholeEmitter.toBytes()],
       wormhole
     );
-
-    console.log(payer);
 
     console.log("Sending transaction");
     console.log("core bridge", CORE_BRIDGE_ADDRESS.toString());
@@ -119,69 +111,21 @@ class Orchestrator {
       .signers([message_account])
       .rpc();
   }
-
-  deriveSealedTransferMessageAccount(
-    saleId: Buffer,
-    mint: web3.PublicKey
-  ): web3.PublicKey {
-    return deriveAddress(
-      [Buffer.from("bridge-sealed"), saleId, mint.toBytes()],
-      this.program.programId
-    );
-  }
-
-  deriveAttestContributionsMessageAccount(saleId: Buffer): web3.PublicKey {
-    return deriveAddress(
-      [Buffer.from("attest-contributions"), saleId],
-      this.program.programId
-    );
-  }
-
-  deriveSaleAccount(saleId: Buffer): web3.PublicKey {
-    return deriveAddress(
-      [Buffer.from("icco-sale"), saleId],
-      this.program.programId
-    );
-  }
-
-  deriveBuyerAccount(saleId: Buffer, buyer: web3.PublicKey): web3.PublicKey {
-    return deriveAddress(
-      [Buffer.from("icco-buyer"), saleId, buyer.toBuffer()],
-      this.program.programId
-    );
-  }
-
-  deriveSignedVaaAccount(signedVaa: Buffer): web3.PublicKey {
-    const hash = hashVaaPayload(signedVaa);
-    return deriveAddress([Buffer.from("PostedVAA"), hash], this.wormhole);
-  }
-
-  deriveCustodianAccount(): web3.PublicKey {
-    return deriveAddress(
-      [Buffer.from("icco-custodian")],
-      this.program.programId
-    );
-  }
 }
 
-describe("solana", () => {
-  console.log(
-    "==============",
-    tryNativeToHexString(
-      "sKUZWMLJNqnqF9bMHomfbghWz62PNkewxRzGeXrJq35",
-      "solana"
-    )
-  );
+describe("solana", async () => {
+  const { parse_vaa } = await importCoreWasm();
+
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
-  provider.opts.skipPreflight = true;
+  // provider.opts.skipPreflight = true;
   // provider.opts.commitment = "confirmed";
   // provider.opts.preflightCommitment = "confirmed";
   anchor.setProvider(provider);
 
   const program = anchor.workspace.CoreGame as Program<CoreGame>;
 
-  provider.connection.onLogs(program.programId, logListener, "confirmed");
+  // provider.connection.onLogs(program.programId, logListener, "confirmed");
 
   const message_account = anchor.web3.Keypair.generate();
   const owner = provider.wallet;
@@ -198,39 +142,134 @@ describe("solana", () => {
 
     await sleep(2000);
 
-    // console.log(
-    //   "Champion:",
-    //   await program.account.championAccount.fetch(championPda)
-    // );
+    console.log(
+      "Champion:",
+      await program.account.championAccount.fetch(championPda)
+    );
 
     console.log("Your transaction signature", tx);
-    // console.log(
-    //   "tx",
-    //   await program.provider.connection.getTransaction(tx, {
-    //     commitment: "confirmed",
-    //   })
-    // );
+    console.log(
+      "tx",
+      await program.provider.connection.getTransaction(tx, {
+        commitment: "confirmed",
+      })
+    );
 
     const seq = parseSequencesFromLogSolana(
       await program.provider.connection.getTransaction(tx, {
         commitment: "confirmed",
       })
     );
-    // console.log("Sequence: ", seq);
+    console.log("Sequence: ", seq);
     const emitterAddress = await getEmitterAddressSolana(
       program.programId.toString()
     );
-    // console.log("Emitter Address: ", emitterAddress);
+    console.log("Emitter Address: ", emitterAddress);
 
     const WH_DEVNET_REST = "http://localhost:7071";
     const url = `${WH_DEVNET_REST}/v1/signed_vaa/${CHAIN_ID_SOLANA}/${emitterAddress}/${seq}`;
-    // console.log(url);
+    console.log(url);
 
     let response = await axios.get(url);
 
-    // console.log(response);
+    let vaa = response.data.vaaBytes;
+    let vaaBytes = Buffer.from(vaa, "base64");
 
+    console.log("vaa", vaa);
+
+    await postVaaSolanaWithRetry(
+      provider.connection,
+      async (tx) => {
+        await provider.wallet.signTransaction(tx);
+        // tx.partialSign(provider.wallet);
+        return tx;
+      },
+      SOLANA_CORE_BRIDGE_ADDRESS,
+      provider.wallet.publicKey.toString(),
+      vaaBytes,
+      10
+    );
+
+    console.log("Vaa Posted");
     await sleep(5000);
+
+    const parsed_vaa = parse_vaa(vaaBytes);
+
+    if (true) {
+      let emitter_address_acc = findProgramAddressSync(
+        [
+          Buffer.from("EmitterAddress"),
+          b.serializeUint16(parsed_vaa.emitter_chain),
+        ],
+        program.programId
+      )[0];
+
+      let processed_vaa_key = findProgramAddressSync(
+        [
+          Buffer.from(emitterAddress, "hex"),
+          b.serializeUint16(parsed_vaa.emitter_chain),
+          b.serializeUint64(parsed_vaa.sequence),
+        ],
+        program.programId
+      )[0];
+
+      //Create VAA Hash to use in core bridge key
+      let buffer_array = [];
+      buffer_array.push(b.serializeUint32(parsed_vaa.timestamp));
+      buffer_array.push(b.serializeUint32(parsed_vaa.nonce));
+      buffer_array.push(b.serializeUint16(parsed_vaa.emitter_chain));
+      buffer_array.push(Uint8Array.from(parsed_vaa.emitter_address));
+      buffer_array.push(b.serializeUint64(parsed_vaa.sequence));
+      buffer_array.push(b.serializeUint8(parsed_vaa.consistency_level));
+      buffer_array.push(Uint8Array.from(parsed_vaa.payload));
+      const hash = keccak256(Buffer.concat(buffer_array));
+
+      let core_bridge_vaa_key = findProgramAddressSync(
+        [Buffer.from("PostedVAA"), hash],
+        new anchor.web3.PublicKey(SOLANA_CORE_BRIDGE_ADDRESS)
+      )[0];
+      console.log("Core Bridge VAA Key: ", core_bridge_vaa_key.toString());
+
+      let config_acc = findProgramAddressSync(
+        [Buffer.from("config")],
+        program.programId
+      )[0];
+
+      if (false) {
+        //Confirm via Messenger Code
+        // await program.methods
+        //   .confirmMsg()
+        //   .accounts({
+        //     payer: KEYPAIR.publicKey,
+        //     systemProgram: anchor.web3.SystemProgram.programId,
+        //     processedVaa: processed_vaa_key,
+        //     emitterAcc: emitter_address_acc,
+        //     coreBridgeVaa: core_bridge_vaa_key,
+        //     config: config_acc,
+        //   })
+        //   .rpc();
+
+        // console.log(
+        //   (await program.account.config.fetch(config_acc)).currentMsg
+        // );
+      } else {
+        const signature = await program.methods
+          .debug()
+          .accounts({ owner: owner.publicKey, coreBridgeVaa: core_bridge_vaa_key})
+          .rpc();
+        console.log("debug signature", signature);
+
+        await sleep(5000);
+
+        const transaction = await program.provider.connection.getTransaction(
+          signature,
+          {
+            commitment: "confirmed",
+          }
+        );
+        console.log("debug tx:", transaction);
+      }
+    }
   });
 });
 
@@ -238,25 +277,33 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const logListener = async (logs, ctx) => {
-  console.group("Event emitted");
-  console.log(logs);
-  console.log(ctx);
-  const transaction = await provider.connection.getTransaction(logs.signature, {
-    commitment: "confirmed",
-  });
+const logListener = (
+  provider: anchor.AnchorProvider,
+  program: Program<CoreGame>
+) => {
+  return async (logs, ctx) => {
+    console.group("Event emitted");
+    console.log(logs);
+    console.log(ctx);
+    const transaction = await provider.connection.getTransaction(
+      logs.signature,
+      {
+        commitment: "confirmed",
+      }
+    );
 
-  const seq = parseSequencesFromLogSolana(transaction);
-  const emitterAddress = await getEmitterAddressSolana(
-    program.programId.toString()
-  );
-  const WH_DEVNET_REST = "http://localhost:7071";
-  const url = `${WH_DEVNET_REST}/v1/signed_vaa/${CHAIN_ID_SOLANA}/${emitterAddress}/${seq}`;
-  console.log("url", url);
+    const seq = parseSequencesFromLogSolana(transaction);
+    const emitterAddress = await getEmitterAddressSolana(
+      program.programId.toString()
+    );
+    const WH_DEVNET_REST = "http://localhost:7071";
+    const url = `${WH_DEVNET_REST}/v1/signed_vaa/${CHAIN_ID_SOLANA}/${emitterAddress}/${seq}`;
+    console.log("url", url);
 
-  let response = await axios.get(url);
+    let response = await axios.get(url);
 
-  console.log(JSON.stringify(response));
+    console.log(JSON.stringify(response));
 
-  console.groupEnd();
+    console.groupEnd();
+  };
 };
