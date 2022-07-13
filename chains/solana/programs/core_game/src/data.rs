e1f090900fee1182649dabc6bd7070b2bd3097d9
Paul Biberstein <biberstein.paul@gmail.com>
2022-07-13 17:18:00 +0000

    Match solana champion repr to solidity repr

--- chains/solana/programs/core_game/src/data.rs
+++ chains/solana/programs/core_game/src/data.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 use borsh;
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct Champion {
+pub struct ChampionStats {
     attack: u32,
     defense: u32,
     speed: u32,
@@ -11,6 +11,26 @@ pub struct Champion {
     xp: u32,
     upgrade_points: u32,
 }
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct AudienceVotes {
+    attack: u32,
+    defense: u32,
+    speed: u32,
+    crit_rate: u32,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Champion {
+    champion_hash: Pubkey,
+    owner: Pubkey,
+    uri: String,
+    vaa_seq: u64,
+    round: u32,
+    stats: ChampionStats,
+    votes: AudienceVotes,
+}
+
 impl Champion {
     /// given a byte value, creates a stat by dividing the byte value by `scale` and adding `offset`.
     /// e.g. byte_to_stat(n, 2, 5) would produce values in the range [5, n/2 + 5]
@@ -33,18 +53,31 @@ impl Champion {
             bytes[3] >> 4,
         ];
         Self {
-            // attack in range [5,13]
-            attack: Self::byte_to_stat(half_bytes[0], 2, 5),
-            // defense in range [1,4]
-            defense: Self::byte_to_stat(half_bytes[1], 5, 1),
-            // speed in range [1,17]
-            speed: Self::byte_to_stat(half_bytes[2], 1, 1),
-            // crit_rate in range [10,26]
-            crit_rate: Self::byte_to_stat(half_bytes[3], 1, 10),
+            stats: ChampionStats {
+                // attack in range [5,13]
+                attack: Self::byte_to_stat(half_bytes[0], 2, 5),
+                // defense in range [1,4]
+                defense: Self::byte_to_stat(half_bytes[1], 5, 1),
+                // speed in range [1,17]
+                speed: Self::byte_to_stat(half_bytes[2], 1, 1),
+                // crit_rate in range [10,26]
+                crit_rate: Self::byte_to_stat(half_bytes[3], 1, 10),
 
-            level: 1,
-            xp: 0,
-            upgrade_points: 1,
+                level: 1,
+                xp: 0,
+                upgrade_points: 1,
+            },
+            votes: AudienceVotes {
+                attack: 0,
+                defense: 0,
+                speed: 0,
+                crit_rate: 0,
+            },
+            champion_hash: Pubkey::default(),
+            owner: Pubkey::default(),
+            uri: "".to_string(),
+            vaa_seq: 0,
+            round: 0,
         }
     }
 }
@@ -62,17 +95,17 @@ pub struct WormholeMessageAccount {
 
 #[account]
 #[derive(Default)]
-pub struct Config{
+pub struct Config {
     pub owner: Pubkey,
     pub nonce: u32,
-    pub current_msg: String
+    pub current_msg: String,
 }
 
 #[account]
 #[derive(Default)]
-pub struct EmitterAddrAccount{
+pub struct EmitterAddrAccount {
     pub chain_id: u16,
-    pub emitter_addr: String
+    pub emitter_addr: String,
 }
 
 //Empty account, we just need to check that it *exists*
