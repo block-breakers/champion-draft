ef1a31a4e7af459d068bbd5ccd0506917f03dced
Paul Biberstein <biberstein.paul@gmail.com>
2022-07-13 14:40:58 +0000

    Fix url in _app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -76,7 +76,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const network = WalletAdapterNetwork.Devnet;
 
   // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const endpoint = "http://localhost:8899";
 
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies
